f6db5b1e195302e4b6d9480cf96f3fe17d248382
Lars Maechel <maechel@users.noreply.github.com>
2026-01-08 20:39:03 +0100

    feat: add initial styles, role definitions, and event management interfaces

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
     {
+        ignores: ['dist/**', 'node_modules/**', 'playwright-report/**', 'test-results/**'],
+    },
+    {
         files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
         plugins: { js },
         extends: ['js/recommended'],
@@ -16,11 +19,16 @@ export default defineConfig([
     tseslint.configs.recommended,
     pluginVue.configs['flat/essential'],
     {
-        files: ['**/*.vue'],
+        files: ['**/*.{ts,vue}'],
         languageOptions: {
             parserOptions: {
                 parser: tseslint.parser,
             },
+        },
+        rules: {
+            'no-undef': 'off',
+            'vue/multi-word-component-names': 'off',
+            'vue/no-reserved-component-names': 'off',
         },
     },
 ]);
